d3a35422df62878cfa0d09fd1f0921d963393fa8
FraBona <francescobonafede08@gmail.com>
2024-01-09 17:28:45 +0100

    create table and type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['Informatica', 'Scienza', 'Storia', 'Medicina'];
+
+        foreach($types as $type){
+            $new_type = new Type();
+
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($type);
+
+            $new_type->save();
+        }
     }
 }
